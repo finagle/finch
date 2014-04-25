910603558a3d7e40039c0bf4c635f24ad727a287
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2014-04-25 18:18:52 +0700

    working on #1

--- src/main/scala/com/mycompany/Demo.scala
+++ src/main/scala/com/mycompany/Demo.scala
@@ -1,30 +1,35 @@
-package io.finch
+package com.mycompany
 
 import com.twitter.finagle.http.path._
 import com.twitter.finagle.{Filter, Service}
 import com.twitter.util.Future
 import com.twitter.finagle.http.Method
+import io.finch.{HttpResponse, RestApi, TurnJsonToHttp, WrapJsonWithMetaAsTag, Resource, JsonObject, JsonArray, JsonResponse, HttpRequest}
 
 object GetAllUsers extends Service[HttpRequest, JsonResponse] {
   def apply(request: HttpRequest): Future[JsonResponse] = {
-    JsonArray(
+    val rep = JsonArray(
       JsonObject("id" -> 10, "name" -> "Ivan"),
       JsonObject("id" -> 20, "name" -> "John")
-    ).toFuture
+    )
+    Future.value(rep)
   }
 }
 
 class GetUserById(id: Long) extends Service[HttpRequest, JsonResponse] {
   def apply(request: HttpRequest): Future[JsonResponse] = {
-    JsonObject("id" -> id, "name" -> "Simon").toFuture
+    val rep = JsonObject("id" -> id, "name" -> "Simon")
+    Future.value(rep)
   }
 }
 
 object User extends Resource {
 
   def route = {
-    case Method.Get -> Root / "users" => GetAllUsers afterThat TurnJsonToHttp
-    case Method.Get -> Root / "users" / Long(id) => new GetUserById(id) afterThat TurnJsonToHttp
+    case Method.Get -> Root / "users" =>
+      GetAllUsers afterThat WrapJsonWithMetaAsTag("users") afterThat TurnJsonToHttp
+    case Method.Get -> Root / "users" / Long(id) =>
+      new GetUserById(id) afterThat WrapJsonWithMetaAsTag("user") afterThat TurnJsonToHttp
   }
 }
 
--- src/main/scala/io/finch/package.scala
+++ src/main/scala/io/finch/package.scala
@@ -1,14 +1,22 @@
 package io
 
-import scala.util.parsing.json.{JSONArray, JSONObject}
 import com.twitter.util.Future
 import com.twitter.finagle.{Service, Filter}
-import com.twitter.finagle.http.{Status, Version, Response}
+import com.twitter.finagle.http.service.RoutingService
+import com.twitter.finagle.http.path.Path
+import com.twitter.finagle.builder.ServerBuilder
+import scala.util.parsing.json.{JSONType, JSONArray, JSONObject}
+import java.net.InetSocketAddress
+import org.jboss.netty.handler.codec.http.HttpMethod
+import scala.util.Random
+import com.twitter.finagle.http.{Http, Status, Version, Response, Request, RichHttp}
 
 package object finch {
-  type HttpRequest = com.twitter.finagle.http.Request
-  type HttpResponse = com.twitter.finagle.http.Response
-  type JsonResponse = scala.util.parsing.json.JSONType
+  type HttpRequest = Request
+  type HttpResponse = Response
+  type JsonResponse = JSONType
+  type HttpServiceOf[+Rep] = Service[HttpRequest, Rep]
+  type HttpService = HttpServiceOf[HttpResponse]
 
   object JsonObject {
     def apply(args: (String, Any)*) = JSONObject(args.toMap)
@@ -18,20 +26,97 @@ package object finch {
     def apply(args: JSONObject*) = JSONArray(args.toList)
   }
 
-  implicit class JsonToFuture(json: JsonResponse) {
-      def toFuture: Future[JsonResponse] = Future.value(json)
-  }
-
   trait Facet[+RepIn, -RepOut] extends Filter[HttpRequest, RepOut, HttpRequest, RepIn]
 
   object TurnJsonToHttp extends Facet[JsonResponse, HttpResponse] {
     def apply(req: HttpRequest, service: Service[HttpRequest, JsonResponse]): Future[HttpResponse] =
-      service(req) flatMap { rep =>
+      service(req) flatMap { json =>
         val rep = Response(Version.Http11, Status.Ok)
         rep.setContentTypeJson()
-        rep.setContentString(rep.toString())
+        rep.setContentString(json.toString())
 
         Future.value(rep)
       }
+  }
+
+  class WrapJsonWithMetaAsTag(tag: String) extends Facet[JsonResponse, JsonResponse] {
+    def apply(req: HttpRequest, service: Service[HttpRequest, JsonResponse]): Future[JsonResponse] =
+      service(req) flatMap { json =>
+        val rep = JsonObject(
+          // TODO: status value
+          "status" -> 200,
+          tag -> json
+        )
+
+        Future.value(rep)
+      }
+  }
+
+  object WrapJsonWithMetaAsTag {
+    def apply(tag: String) = new WrapJsonWithMetaAsTag(tag)
+  }
+
+  trait Resource { self =>
+
+    /**
+     * Returns a route itself.
+     *
+     * @return
+     */
+    def route: PartialFunction[(HttpMethod, Path), Service[HttpRequest, HttpResponse]]
+
+    /**
+     *
+     * @param that
+     * @return
+     */
+    def orElse(that: Resource) = new Resource {
+      def route = self.route orElse that.route
+    }
+
+    /**
+     *
+     * @param f
+     * @return
+     */
+    def andThen(f: Service[HttpRequest, HttpResponse] => Service[HttpRequest, HttpResponse]) =
+      new Resource {
+        def route = self.route andThen f
+      }
+
+    implicit class AfterThatService[+RepIn](service: => Service[HttpRequest, RepIn]) {
+      def afterThat[A](thatFacet: Facet[RepIn, A]) =
+        thatFacet andThen service
+    }
+
+    implicit class AfterThatFacet[+RepIn, -RepOut](facet: => Facet[RepIn, RepOut]) {
+      def afterThat[A](thatFacet: Facet[RepOut, A]) =
+        thatFacet andThen facet
+    }
+  }
+
+
+  class RestApi extends App {
+
+    implicit class FilterAndThenResource(filter: Filter[HttpRequest, HttpResponse, HttpRequest, HttpResponse]) {
+      def andThen(resource: => Resource) = resource andThen { filter andThen _ }
+    }
+
+    def name = "FinchInstance-" + new Random().alphanumeric.take(20)
+
+    def exposeAt(port: Int)(resource: => Resource): Unit = {
+
+      val service = new RoutingService[HttpRequest](
+        new PartialFunction[HttpRequest, Service[HttpRequest, HttpResponse]] {
+          def apply(req: HttpRequest) = resource.route(req.method -> Path(req.path))
+          def isDefinedAt(req: HttpRequest) = resource.route.isDefinedAt(req.method -> Path(req.path))
+        })
+
+      ServerBuilder()
+        .codec(RichHttp[HttpRequest](Http()))
+        .bindTo(new InetSocketAddress(port))
+        .name(name)
+        .build(service)
+    }
   }
 }
