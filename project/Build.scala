6817c0a4165beeb57f89ce0acaed2eb88fb983b3
Ryan Plessner <plessner.ryan@gmail.com>
2014-08-11 21:28:55 -0400

    added the scalatest dependency

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,8 @@ object Finch extends Build {
 
   val baseSettings = Defaults.defaultSettings ++ Seq(
     libraryDependencies ++= Seq(
-      "com.twitter" %% "finagle-http" % "6.18.0"
+      "com.twitter" %% "finagle-http" % "6.18.0",
+      "org.scalatest" % "scalatest_2.10" % "2.2.1" % "test"
     ),
     scalacOptions ++= Seq( "-unchecked", "-deprecation", "-feature")
   )
