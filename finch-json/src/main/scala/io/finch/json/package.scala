9032db69c43c175a5c61dd2b1c1ff90c1a17d916
Vladimir Kostyukov <vkostyukov@twitter.com>
2014-11-26 11:26:44 -0800

    Fix encode error + license header

--- finch-json/src/main/scala/io/finch/json/package.scala
+++ finch-json/src/main/scala/io/finch/json/package.scala
@@ -1,5 +1,30 @@
+/*
+ * Copyright 2014, by Vladimir Kostyukov and Contributors.
+ *
+ * This file is a part of a Finch library that may be found at
+ *
+ *      https://github.com/finagle/finch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributor(s):
+ */
+
 package io.finch
 
+/**
+ * This package contains the default JSON implementation for the Finch.io library.
+ */
 package object json {
 
   /**
@@ -118,7 +143,7 @@ package object json {
 
       def wire(any: Any): String = any match {
         case s: String => escape(s)
-        case JsonObject(map) => "{" + map.map({ case (k,v) => wire(k.toString) + " : " + wire(v) }) + "}"
+        case JsonObject(map) => "{" + map.map({ case (k, v) => wire(k.toString) + ":" + wire(v) }).mkString(",") + "}"
         case JsonArray(list) => "[" + list.map(wire).mkString(",") + "]"
         case other => other.toString
       }
