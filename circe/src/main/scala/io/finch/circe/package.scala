51107bdbf70643fac3700de1e93ca7c5522cfd3c
Mohammed Najim <mtnajim@gmail.com>
2015-12-12 11:40:59 +0000

    Add Jackson serialiser for Circe

--- circe/src/main/scala/io/finch/circe/package.scala
+++ circe/src/main/scala/io/finch/circe/package.scala
@@ -1,16 +1,28 @@
 package io.finch
 
-import io.circe.Printer
+import io.circe.{Json, Printer}
 
 package object circe extends Encoders with Decoders {
 
-  override protected val printer: Printer = Printer.noSpaces
+  override protected def print(json: Json): String = Printer.noSpaces.pretty(json)
 
   /**
    * Provides an implicit [[Printer]] that drops null keys.
    */
   object dropNullKeys extends Encoders with Decoders {
-    override protected val printer: Printer = Printer.noSpaces.copy(dropNullKeys = true)
+
+    private val printer: Printer = Printer.noSpaces.copy(dropNullKeys = true)
+
+    override protected def print(json: Json): String = printer.pretty(json)
   }
+
+  /**
+   * Jackson Serializer
+   */
+  object jacksonSerializer extends Encoders with Decoders {
+
+    override protected def print(json: Json): String = io.circe.jackson.jacksonPrint(json)
+  }
+
 }
 
