aeeb9f2656a19e2068f3e37d4ec0f802fc34efc2
Barry Kern <barryk@slalom.com>
2016-02-19 07:50:42 -0600

    Adding explicit types on readers and writers

--- playjson/src/test/scala/io/finch/playjson/Bar.scala
+++ playjson/src/test/scala/io/finch/playjson/Bar.scala
@@ -4,6 +4,6 @@ import play.api.libs.json._
 case class Bar(x: Int, y: Boolean)
 
 object Bar {
-  implicit val barWrites = Json.writes[Bar]
-  implicit val barReads = Json.reads[Bar]
+  implicit val barWrites: Writes[Bar] = Json.writes[Bar]
+  implicit val barReads: Reads[Bar] = Json.reads[Bar]
 }
