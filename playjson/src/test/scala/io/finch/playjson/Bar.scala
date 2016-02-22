d32f494a4fdc213ad453b41d92a3d55a6ead00f1
Barry Kern <barryk@slalom.com>
2016-02-22 11:29:38 -0600

    removing Bar.scala updating test but still need more feedback

--- playjson/src/test/scala/io/finch/playjson/Bar.scala
+++
@@ -1,9 +0,0 @@
-package io.finch.playjson
-
-import play.api.libs.json._
-case class Bar(x: Int, y: Boolean)
-
-object Bar {
-  implicit val barWrites: Writes[Bar] = Json.writes[Bar]
-  implicit val barReads: Reads[Bar] = Json.reads[Bar]
-}
--- playjson/src/test/scala/io/finch/playjson/PlayJsonSpec.scala
+++ playjson/src/test/scala/io/finch/playjson/PlayJsonSpec.scala
@@ -1,12 +1,19 @@
 package io.finch.playjson
 
 import io.finch.test.json.JsonCodecProviderProperties
+import io.finch.test.json.ExampleCaseClass
+import io.finch.test.json.ExampleNestedCaseClass
 import org.scalatest.prop.Checkers
 import org.scalatest.{FlatSpec, Matchers}
+import play.api.libs.json._
 
 class PlayJsonSpec extends FlatSpec with Matchers with Checkers with JsonCodecProviderProperties {
+    implicit val exampleReads:Reads[ExampleCaseClass] = Json.reads[ExampleCaseClass]
+    implicit val exampleWrites:Writes[ExampleCaseClass] = Json.writes[ExampleCaseClass]
 
-  implicit val formats = play.api.libs.json.Format
+    implicit val exampleNestedReads:Reads[ExampleNestedCaseClass] = Json.reads[ExampleNestedCaseClass]
+    implicit val exampleNestedWrites:Writes[ExampleNestedCaseClass] = Json.writes[ExampleNestedCaseClass]
+    
   "The PlayJson codec provider" should "encode a case class as JSON" in encodeNestedCaseClass
   it should "decode a case class from JSON" in decodeNestedCaseClass
   it should "properly fail to decode invalid JSON into a case class" in failToDecodeInvalidJson
