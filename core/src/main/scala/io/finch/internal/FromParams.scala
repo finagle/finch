a74fc025e3a76981f641292709e0b648a64d110f
ImLiar <whoisliar@gmail.com>
2016-03-24 23:56:28 +0300

    FromParams should be tolerant to optional and seq fields

--- core/src/main/scala/io/finch/internal/FromParams.scala
+++ core/src/main/scala/io/finch/internal/FromParams.scala
@@ -3,8 +3,9 @@ package io.finch.internal
 import scala.reflect.ClassTag
 
 import io.finch._
-import shapeless.{::, HList, HNil, Witness}
+import shapeless._
 import shapeless.labelled._
+import shapeless.poly._
 
 /**
  * A type class empowering a generic derivation of [[Endpoint]]s from query string params.
@@ -20,13 +21,36 @@ object FromParams {
   }
 
   implicit def hconsFromParams[HK <: Symbol, HV, T <: HList](implicit
-    dh: Decode[HV],
-    ct: ClassTag[HV],
     key: Witness.Aux[HK],
-    fpt: FromParams[T]
+    fpt: FromParams[T],
+    hс: Case1.Aux[Extractor.type, String, Endpoint[HV]]
   ): FromParams[FieldType[HK, HV] :: T] = new FromParams[FieldType[HK, HV] :: T] {
-    def endpoint: Endpoint[FieldType[HK, HV] :: T] =
-      param(key.value.name).as(dh, ct).map(field[HK](_)) :: fpt.endpoint
+    def endpoint: Endpoint[FieldType[HK, HV] :: T] = {
+      hс(key.value.name).map(field[HK](_)) :: fpt.endpoint
+    }
   }
 }
 
+private[internal] object Extractor extends Poly1 {
+
+  implicit def optionalExtractor[V](implicit
+    dh: Decode[V],
+    ct: ClassTag[V]
+  ): Case.Aux[String, Endpoint[Option[V]]] = at[String] { key =>
+    paramOption(key).as(dh, ct)
+  }
+
+  implicit def collectionExtractor[V](implicit
+    dh: Decode[V],
+    ct: ClassTag[V]
+  ): Case.Aux[String, Endpoint[Seq[V]]] = at[String] { key =>
+    params(key).as(dh, ct)
+  }
+
+  implicit def extractor[V](implicit
+    dh: Decode[V],
+    ct: ClassTag[V]
+  ): Case.Aux[String, Endpoint[V]] = at[String] { key =>
+    param(key).as(dh, ct)
+  }
+}
