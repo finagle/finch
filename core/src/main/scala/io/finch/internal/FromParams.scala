def076d2461f918b5332a36c183eca92e8062979
Teodimoff <teodimoff@gmail.com>
2016-04-18 16:20:15 +0300

    Add lifted version of nestedExtractor to allow nested structure be Optional.

--- core/src/main/scala/io/finch/internal/FromParams.scala
+++ core/src/main/scala/io/finch/internal/FromParams.scala
@@ -41,6 +41,13 @@ private[internal] object Extractor extends Poly1 {
   ): Case.Aux[String,Endpoint[V]] = at[String]{key =>
     derive[V].fromParams
   }
+  
+  implicit def nestedExtractorLifted[Repr <: HList,V](implicit
+    gen: LabelledGeneric.Aux[V, Repr],
+    fp:  FromParams[Repr]
+  ): Case.Aux[String,Endpoint[Option[V]]] = at[String]{key =>
+    derive[V].fromParams.lift
+  }  
 
   implicit def optionalExtractor[V](implicit
     dh: Decode[V],
