dae3d043b58385d9cac03b13b9081d07ecae8f81
Vladimir Kostyukov <vkostyukov@twitter.com>
2017-04-24 13:39:46 -0700

    Revert Binary Printing for Circe

--- circe/src/main/scala/io/finch/circe/Encoders.scala
+++ circe/src/main/scala/io/finch/circe/Encoders.scala
@@ -3,19 +3,15 @@ package io.finch.circe
 import com.twitter.io.Buf
 import io.circe.{Encoder, Json}
 import io.finch.Encode
-import java.nio.ByteBuffer
-import java.nio.charset.StandardCharsets
 
 trait Encoders {
 
   protected def printString(json: Json): String
-  protected def printBytes(json: Json): ByteBuffer
 
   /**
    * Maps Circe's [[Encoder]] to Finch's [[Encode]].
    */
   implicit def encodeCirce[A](implicit e: Encoder[A]): Encode.Json[A] = Encode.json {
-    case (a, StandardCharsets.UTF_8) => Buf.ByteBuffer.Owned(printBytes(e(a)))
     case (a, cs) => Buf.ByteArray.Owned(printString(e(a)).getBytes(cs.name))
   }
 
--- circe/src/main/scala/io/finch/circe/package.scala
+++ circe/src/main/scala/io/finch/circe/package.scala
@@ -1,12 +1,10 @@
 package io.finch
 
 import io.circe.{Json, Printer}
-import java.nio.ByteBuffer
 
 package object circe extends Encoders with Decoders {
 
   override protected def printString(json: Json): String = Printer.noSpaces.pretty(json)
-  override protected def printBytes(json: Json): ByteBuffer = Printer.noSpaces.prettyByteBuffer(json)
 
   /**
    * Provides a [[Printer]] that drops null keys.
@@ -14,7 +12,6 @@ package object circe extends Encoders with Decoders {
   object dropNullKeys extends Encoders with Decoders {
     private[this] val printer: Printer = Printer.noSpaces.copy(dropNullKeys = true)
     override protected def printString(json: Json): String = printer.pretty(json)
-    override protected def printBytes(json: Json): ByteBuffer = printer.prettyByteBuffer(json)
   }
 
   /**
@@ -23,8 +20,6 @@ package object circe extends Encoders with Decoders {
   object jacksonSerializer extends Encoders with Decoders {
     override protected def printString(json: Json): String =
       io.circe.jackson.jacksonPrint(json)
-    override protected def printBytes(json: Json): ByteBuffer =
-      io.circe.jackson.jacksonPrintByteBuffer(json)
   }
 }
 
