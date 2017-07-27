406c072d8d40ab285f260a3593b08572dbf70493
Gabriel Francisco dos Santos Silva <gabfssilva@gmail.com>
2017-07-27 15:45:42 -0300

    Removing unused functions

--- core/src/main/scala/io/finch/internal/ScalaToTwitterConversions.scala
+++ core/src/main/scala/io/finch/internal/ScalaToTwitterConversions.scala
@@ -5,18 +5,10 @@ import com.twitter.util._
 private[finch] object ScalaToTwitterConversions {
   import scala.concurrent.ExecutionContext
 
-  implicit def asTwitterFuture[F, T](f: F => scala.concurrent.Future[T])
-                                    (implicit ec: ExecutionContext): F => Future[T] = {
-    first: F => f.apply(first).asTwitterFuture
-  }
-
   implicit def scalaToTwitterTry[T](t: scala.util.Try[T]): Try[T] = t match {
     case scala.util.Success(r) => Return(r)
     case scala.util.Failure(ex) => Throw(ex)
   }
-
-  implicit def asTwitterFuture[T](f: scala.concurrent.Future[T])(implicit ec: ExecutionContext)
-    : Future[T] = f.asTwitterFuture
 
   implicit class ScalaTwitterConvertionsImplicits[T](f: scala.concurrent.Future[T]) {
     def asTwitterFuture(implicit ec: ExecutionContext): Future[T] = {
