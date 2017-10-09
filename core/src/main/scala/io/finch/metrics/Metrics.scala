e27a84e5a983996d51fa9ef045af61b4f229522a
skolbasov <sergey.kolbasov@zalando.fi>
2017-10-09 11:35:18 +0300

    Fix docs

--- core/src/main/scala/io/finch/metrics/Metrics.scala
+++ core/src/main/scala/io/finch/metrics/Metrics.scala
@@ -3,32 +3,32 @@ package io.finch.metrics
 trait Metrics {
 
   /**
-    * Count a call to endpoint by path and method
+    * Count call to endpoint by the path and method
     */
   def countCall(path: String): Unit
 
   /**
-    * Count failure if exception was thrown during processing request to an endpoint
+    * Count failure if exception was thrown during processing a request to an endpoint
     */
   def countFailure(path: String, e: Throwable): Unit
 
   /**
-    * Count time that was required to process failed request
+    * Count time that was required to process a failed request
     */
   def failureTime(path: String, e: Throwable, duration: Float): Unit
 
   /**
-    * Count success if request was processed without exceptions
+    * Count success if a request was processed without exceptions
     */
   def countSuccess(path: String, code: Int): Unit
 
   /**
-    * Count
+    * Count time that was required to process a successful request
     */
   def successTime(path: String, code: Int, duration: Float): Unit
 
   /**
-    * Count size of response
+    * Count size of a response
     */
   def size(path: String, s: Float): Unit
 
