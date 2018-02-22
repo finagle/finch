d5fae74bda24507c877fb4fddb357c80f52bec21
Nepomuk Seiler <muuki88@users.noreply.github.com>
2018-02-22 02:01:01 +0100

    Move contentTypes into separate file (#918)

--- core/src/main/scala/io/finch/package.scala
+++ core/src/main/scala/io/finch/package.scala
@@ -1,7 +1,5 @@
 package io
 
-import shapeless.Witness
-
 /**
  * This is a root package of the Finch library, which provides an immutable layer of functions and
  * types atop of Finagle for writing lightweight HTTP services.
@@ -21,20 +19,4 @@ package object finch extends Endpoints
     case object MultipleItems extends RequestItem("request")
   }
 
-  object Application {
-    type Json = Witness.`"application/json"`.T
-    type Xml = Witness.`"application/xml"`.T
-    type AtomXml = Witness.`"application/atom+xml"`.T
-    type Csv = Witness.`"application/csv"`.T
-    type Javascript = Witness.`"application/javascript"`.T
-    type OctetStream = Witness.`"application/octet-stream"`.T
-    type RssXml = Witness.`"application/rss+xml"`.T
-    type WwwFormUrlencoded = Witness.`"application/x-www-form-urlencoded"`.T
-  }
-
-  object Text {
-    type Plain = Witness.`"text/plain"`.T
-    type Html = Witness.`"text/html"`.T
-    type EventStream = Witness.`"text/event-stream"`.T
-  }
 }
