54021176297f3a82e98d9495c17398017798f90e
skolbasov <sergey.kolbasov@zalando.fi>
2018-06-12 00:00:37 +0300

    Generalize decoders for any RefType

--- refined/src/main/scala/io/finch/refined/package.scala
+++ refined/src/main/scala/io/finch/refined/package.scala
@@ -1,16 +1,22 @@
 package io.finch
 
 import com.twitter.util.{Return, Throw}
-import eu.timepit.refined._
-import eu.timepit.refined.api.{Refined, Validate}
+import eu.timepit.refined.api.{RefType, Validate}
 
 package object refined {
 
-  implicit def decodePathRefined[A, B](implicit ad: DecodePath[A], v: Validate[A, B]): DecodePath[A Refined B] =
-    DecodePath.instance(s => ad(s).flatMap(p => refineV[B](p).toOption))
+  implicit def decodePathRefined[F[_, _], A, B](implicit
+    ad: DecodePath[A],
+    v: Validate[A, B],
+    rt: RefType[F]
+  ): DecodePath[F[A, B]] = DecodePath.instance(s => ad(s).flatMap(p => rt.refine[B](p).toOption))
 
-  implicit def decodeEntityRefined[A, B](implicit ad: DecodeEntity[A], v: Validate[A, B]): DecodeEntity[A Refined B] =
-    DecodeEntity.instance(s => ad(s).flatMap(e => refineV[B](e) match {
+  implicit def decodeEntityRefined[F[_, _], A, B](implicit
+    ad: DecodeEntity[A],
+    v: Validate[A, B],
+    rt: RefType[F]
+  ): DecodeEntity[F[A, B]] =
+    DecodeEntity.instance(s => ad(s).flatMap(e => rt.refine[B](e) match {
       case Left(error) => Throw(PredicateFailed(error))
       case Right(ref) => Return(ref)
     }))
