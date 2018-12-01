36caa114eea526df389157bae0f985e213164e57
Jichao Ouyang <oyanglulu@gmail.com>
2018-12-01 19:04:41 +1100

    Revert "scala fix"
    This reverts commit df2e2ae176e7f9230508886dd724e180a2f2d764.

--- core/src/main/scala/io/finch/internal/Mapper.scala
+++ core/src/main/scala/io/finch/internal/Mapper.scala
@@ -1,16 +1,13 @@
 package io.finch.internal
 
-import cats.Monad
+import cats.{~>, Monad}
 import cats.effect.Effect
 import cats.syntax.functor._
 import com.twitter.finagle.http.Response
-import com.twitter.util.{Future => TwitterFuture, Return, Throw}
 import io.finch.{Endpoint, Output}
-import scala.concurrent.{Future => ScalaFuture}
-import scala.util.{Failure, Success}
 import shapeless.HNil
 import shapeless.ops.function.FnToProduct
-import cats.~>
+
 /**
  * A type class that allows the [[Endpoint]] to be mapped to either `A => B` or `A => Future[B]`.
  * @groupname LowPriorityMapper Low Priority Mapper Conversions
@@ -31,34 +28,6 @@ private[finch] trait LowPriorityMapperConversions {
   def instance[F[_], A, B](f: Endpoint[F, A] => Endpoint[F, B]): Mapper.Aux[F, A, B] = new Mapper[F, A] {
     type Out = B
     def apply(e: Endpoint[F, A]): Endpoint[F, B] = f(e)
-  }
-
-  implicit val twitterFutureToEffect[F[_]: Effect] = new (TwitterFuture ~> F) {
-    def apply[A](f: => TwitterFuture[A]): F[A] = {
-      implicitly[Effect[F]].async { cb =>
-        f.respond {
-          case Return(r) => cb(Right(r))
-          case Throw(t) => cb(Left(t))
-        }
-      }
-    }
-  }
-
-  implicit val scalaFutureToEffect[F[_]: Effect] = new (ScalaFuture ~> F) {
-    def apply[A](f: => ScalaFuture[A]): F[A] = {
-      implicitly[Effect[F]].async { cb =>
-        f.onComplete {
-          case Success(s) => cb(Right(s))
-          case Failure(t) => cb(Left(t))
-        }(DummyExecutionContext)
-      }
-    }
-  }
-
-  implicit def mapperToEffectOutputFunction[A, B, F[_], G[_]:Effect](f: A => F[Output[B]])(implicit cov: F ~> G): Mapper.Aux[G, A, B] =
-    instance(_.mapOutputAsync(a => cov.apply(f(a))))
-
-  implicit def mapperToEffectOutputValue[A, B, F[_], G[_]:Effect](f: => F[Output[B]])(implicit cov: F ~> G): Mapper.Aux[G, A, B] = instance(_.mapOutputAsync(a => cov.apply(f)))
   }
 
   /**
@@ -85,23 +54,9 @@ private[finch] trait LowPriorityMapperConversions {
   implicit def mapperFromEffectResponseFunction[A, F[_] : Monad](f: A => F[Response]): Mapper.Aux[F, A, Response] =
     instance(_.mapOutputAsync(f.andThen(fr => fr.map(r => Output.payload(r, r.status)))))
 
-  /**
-    * @group LowPriorityMapper
-    */
-  @deprecated("com.twitter.util.Future use is deprecated in Endpoints. Consider to use cats-effect compatible effect", "0.25.0")
-  implicit def mapperFromFutureResponseFunction[A, F[_] : Effect](f: A => TwitterFuture[Response]): Mapper.Aux[F, A, Response] =
-    instance(_.mapOutputAsync(f.andThen(fr => twitterFutureToEffect(fr).map(r => Output.payload(r, r.status)))))
-
-  /**
-    * @group LowPriorityMapper
-    */
-  @deprecated("scala.concurrent.Future use is deprecated in Endpoints. Consider to use cats-effect compatible effect", "0.25.0")
-  implicit def mapperFromScFutureResponseFunction[A, F[_] : Effect](f: A => ScalaFuture[Response]): Mapper.Aux[F, A, Response] =
-    instance(_.mapOutputAsync(f.andThen(fr => scalaFutureToEffect(fr).map(r => Output.payload(r, r.status)))))
 }
 
 private[finch] trait HighPriorityMapperConversions extends LowPriorityMapperConversions {
-
   /**
     * @group HighPriorityMapper
     */
@@ -146,27 +101,39 @@ private[finch] trait HighPriorityMapperConversions extends LowPriorityMapperConv
   implicit def mapperFromEffectResponseValue[F[_] : Monad](fr: F[Response]): Mapper.Aux[F, HNil, Response] =
     instance(_.mapOutputAsync(_ => fr.map(r => Output.payload(r, r.status))))
 
-  /**
-    * @group HighPriorityMapper
-    */
-  @deprecated("com.twitter.util.Future use is deprecated in Endpoints. Consider to use cats-effect compatible effect", "0.25.0")
-  implicit def mapperFromFutureResponseValue[F[_] : Effect](fr: => TwitterFuture[Response]): Mapper.Aux[F, HNil, Response] =
-    instance(_.mapOutputAsync(_ => twitterFutureToEffect(fr).map(r => Output.payload(r, r.status))))
+  implicit def mapperFromKindToEffectOutputFunction[A, B, F[_], G[_]: Effect](f: A => F[Output[B]])(
+    implicit conv: F ~> G): Mapper.Aux[G, A, B] =
+    instance(_.mapOutputAsync(a => conv.apply(f(a))))
 
-  /**
-    * @group HighPriorityMapper
-    */
-  @deprecated("scala.concurrent.Future use is deprecated in Endpoints. Consider to use cats-effect compatible effect", "0.25.0")
-  implicit def mapperFromScFutureResponseValue[F[_] : Effect](fr: => ScalaFuture[Response]): Mapper.Aux[F, HNil, Response] =
-    instance(_.mapOutputAsync(_ => scalaFutureToEffect(fr).map(r => Output.payload(r, r.status))))
+  implicit def mapperFromKindToEffectOutputValue[A, B, F[_], G[_]: Effect](f: => F[Output[B]])(
+    implicit conv: F ~> G): Mapper.Aux[G, A, B] = instance(_.mapOutputAsync(a => conv.apply(f)))
+
+  implicit def mapperFromKindToEffectResponsFunction[A, F[_], G[_]: Effect](f: A => F[Response])(
+    implicit conv: F ~> G): Mapper.Aux[G, A, Response] =
+    instance(_.mapOutputAsync(f.andThen(fr => conv(fr).map(r => Output.payload(r, r.status)))))
+
+  implicit def mapperFromKindToEffectResponseValue[A, F[_], G[_]: Effect](f: => F[Response])(
+    implicit conv: F ~> G): Mapper.Aux[G, A, Response] =
+    instance(_.mapOutputAsync(_=>conv(f).map(r => Output.payload(r, r.status))))
 }
 
 object Mapper extends HighPriorityMapperConversions {
-  implicit def mapperFromKindOutputHFunction[F[_] : Effect, G[_], A, B, FN, FOB](f: FN)(implicit
-    conv: G ~> F,
+
+  implicit def mapperFromKindOutputHFunction[F[_]: Effect, G[_], A, B, FN, FOB](f: FN)(implicit
     ftp: FnToProduct.Aux[FN, A => FOB],
-    ev: FOB <:< G[Output[B]]
-  ): Mapper.Aux[F, A, B] = instance(_.mapOutputAsync(a => conv.apply(ev(ftp(f)(a)))))
+    ev: FOB <:< G[Output[B]],
+    conv: G ~> F
+  ): Mapper.Aux[F, A, B] =
+    instance(_.mapOutputAsync(a => conv.apply(ev(ftp(f)(a)))))
+
+  implicit def mapperFromKindResponseHFunction[F[_] : Effect, G[_], A, FN, FR](f: FN)(implicit
+    ftp: FnToProduct.Aux[FN, A => FR],
+    ev: FR <:< G[Response],
+    conv: G ~> F
+  ): Mapper.Aux[F, A, Response] = instance(_.mapOutputAsync { value =>
+    val fr = conv(ev(ftp(f)(value)))
+    fr.map(r => Output.payload(r, r.status))
+  })
 
   implicit def mapperFromEffectOutputHFunction[F[_] : Monad, A, B, FN, FOB](f: FN)(implicit
     ftp: FnToProduct.Aux[FN, A => FOB],
@@ -178,36 +145,6 @@ object Mapper extends HighPriorityMapperConversions {
     ev: FR <:< F[Response]
   ): Mapper.Aux[F, A, Response] = instance(_.mapOutputAsync { value =>
     val fr = ev(ftp(f)(value))
-    fr.map(r => Output.payload(r, r.status))
-  })
-
-  @deprecated("com.twitter.util.Future use is deprecated in Endpoints. Consider to use cats-effect compatible effect", "0.25.0")
-  implicit def mapperFromFutureOutputHFunction[F[_] : Effect, A, B, FN, FOB](f: FN)(implicit
-    ftp: FnToProduct.Aux[FN, A => FOB],
-    ev: FOB <:< TwitterFuture[Output[B]]
-  ): Mapper.Aux[F, A, B] = instance(_.mapOutputAsync(value => twitterFutureToEffect(ev(ftp(f)(value)))))
-
-  @deprecated("com.twitter.util.Future use is deprecated in Endpoints. Consider to use cats-effect compatible effect", "0.25.0")
-  implicit def mapperFromFutureResponseHFunction[F[_] : Effect, A, FN, FR](f: FN)(implicit
-    ftp: FnToProduct.Aux[FN, A => FR],
-    ev: FR <:< TwitterFuture[Response]
-  ): Mapper.Aux[F, A, Response] = instance(_.mapOutputAsync { value =>
-    val fr = twitterFutureToEffect(ev(ftp(f)(value)))
-    fr.map(r => Output.payload(r, r.status))
-  })
-
-  @deprecated("scala.concurrent.Future use is deprecated in Endpoints. Consider to use cats-effect compatible effect", "0.25.0")
-  implicit def mapperFromScFutureOutputHFunction[F[_] : Effect, A, B, FN, FOB](f: FN)(implicit
-    ftp: FnToProduct.Aux[FN, A => FOB],
-    ev: FOB <:< ScalaFuture[Output[B]]
-  ): Mapper.Aux[F, A, B] = instance(_.mapOutputAsync(value => scalaFutureToEffect(ev(ftp(f)(value)))))
-
-  @deprecated("scala.concurrent.Future use is deprecated in Endpoints. Consider to use cats-effect compatible effect", "0.25.0")
-  implicit def mapperFromScFutureResponseHFunction[F[_] : Effect, A, FN, FR](f: FN)(implicit
-    ftp: FnToProduct.Aux[FN, A => FR],
-    ev: FR <:< ScalaFuture[Response]
-  ): Mapper.Aux[F, A, Response] = instance(_.mapOutputAsync { value =>
-    val fr = scalaFutureToEffect(ev(ftp(f)(value)))
     fr.map(r => Output.payload(r, r.status))
   })
 }
--- core/src/test/scala/io/finch/MethodSpec.scala
+++ core/src/test/scala/io/finch/MethodSpec.scala
@@ -7,6 +7,8 @@ import org.scalacheck.Arbitrary
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import scala.concurrent.{Future => ScalaFuture}
 import scala.concurrent.ExecutionContext.Implicits.global
+import io.finch.instances.twitterFuture._
+import io.finch.instances.scalaFuture._
 
 class MethodSpec
   extends FinchSpec
@@ -29,6 +31,7 @@ class MethodSpec
   }
 
   it should "map TwitterFuture[Output[A]] value to endpoint" in {
+
     checkValue((i: String) => get(zero) { TwitterFuture.value(Ok(i)) } )
   }
 
@@ -113,6 +116,43 @@ class MethodSpec
   it should "map (A, B) => ScalaFuture[Response] function to endpoint" in {
     checkFunction2(get(path[Int] :: path[Int]) { (x: Int, y: Int) =>
       ScalaFuture.successful(Ok(s"$x$y").toResponse[Text.Plain]) })
+  }
+
+  behavior of "Custom Type Program[_]"
+
+  case class Program[A](value: A)
+
+  import cats.~>
+  implicit val conv: Program ~> IO = new (Program ~> IO) {
+    def apply[A](a: Program[A]): IO[A] = IO(a.value)
+  }
+
+  it should "map Program[Output[_]] value to endpoint" in {
+    checkValue((i: String) => get(zero) { Program(Ok(i)) })
+  }
+
+  it should "map A => Program[Output[_]] function to endpoint" in {
+    checkFunction(get(path[Int]) { i: Int => Program(Ok(i)) })
+  }
+
+  it should "map (A, B) => Program[Output[_]] function to endpoint" in {
+    checkFunction2(get(path[Int] :: path[Int]) { (x: Int, y: Int) =>
+      Program(Ok(s"$x$y"))
+    })
+  }
+
+  it should "map Program[Response] value to endpoint" in {
+    checkValue((i: Response) => get(zero) { Program(i) })
+  }
+
+  it should "map A => Program[Response] function to endpoint" in {
+    checkFunction(get(path[Int]) { i: Int => Program(Ok(i).toResponse[Text.Plain]) })
+  }
+
+  it should "map (A, B) => Program[Response] function to endpoint" in {
+    checkFunction2(get(path[Int] :: path[Int]) { (x: Int, y: Int) =>
+      Program(Ok(s"$x$y").toResponse[Text.Plain])
+    })
   }
 
   private def checkValue[A : Arbitrary](f: A => Endpoint[IO, A]): Unit = {
