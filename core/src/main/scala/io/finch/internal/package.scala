dfcc99786fecb40bd12d903fa1122af298fdcacd
Vladimir Kostyukov <vladimir.v.kostyukov@gmail.com>
2019-03-27 21:57:36 -0700

    Fix bug in charsetOrUtf8

--- core/src/main/scala/io/finch/internal/package.scala
+++ core/src/main/scala/io/finch/internal/package.scala
@@ -6,8 +6,6 @@ import com.twitter.util.Future
 import io.catbird.util.Rerunnable
 import java.nio.ByteBuffer
 import java.nio.charset.{Charset, StandardCharsets}
-import scala.annotation.tailrec
-import shapeless.HNil
 
 /**
  * This package contains an internal-use only type-classes and utilities that power Finch's API.
@@ -77,23 +75,19 @@ package object internal {
 
     // Returns message's charset or UTF-8 if it's not defined.
     def charsetOrUtf8: Charset = {
-
       val contentType = self.headerMap.getOrNull(Fields.ContentType)
 
-      @tailrec
-      def loop(semicolon: Int): Charset =
-        if (semicolon == -1 || semicolon == contentType.length) StandardCharsets.UTF_8
-        else {
-          val next = contentType.indexOf(';', semicolon + 1)
-          if (contentType.startsWith("charset=", semicolon + 1)) {
-            val start = contentType.indexOf('=', semicolon + 1) + 1
-            val end = if (next == -1) contentType.length else next
-            Charset.forName(contentType.substring(start, end))
-          } else loop(next)
-        }
-
       if (contentType == null) StandardCharsets.UTF_8
-      else loop(contentType.indexOf(';'))
+      else {
+        val charsetEq = contentType.indexOf("charset=")
+        if (charsetEq == -1) StandardCharsets.UTF_8
+        else {
+          val from = charsetEq + "charset=".length
+          val semi = contentType.indexOf(';', from)
+          val to = if (semi == -1) contentType.length else semi
+          Charset.forName(contentType.substring(from, to))
+        }
+      }
     }
   }
 
--- core/src/test/scala/io/finch/internal/HttpMessageSpec.scala
+++ core/src/test/scala/io/finch/internal/HttpMessageSpec.scala
@@ -11,11 +11,20 @@ class HttpMessageSpec extends FinchSpec {
     case None => StandardCharsets.UTF_8
   }
 
-  "HttpMessage" should "charsetOrUtf8" in {
+  behavior of "HttpMessage"
+
+  it should "charsetOrUtf8" in {
     check { cs: Charset =>
       val req = Request()
       req.contentType = "application/json"
       req.charset = cs.displayName()
+
+      req.charsetOrUtf8 === slowCharset(req)
+    }
+
+    check { cs: Charset =>
+      val req = Request()
+      req.contentType = "application/json;   charset=" + cs.displayName()
 
       req.charsetOrUtf8 === slowCharset(req)
     }
