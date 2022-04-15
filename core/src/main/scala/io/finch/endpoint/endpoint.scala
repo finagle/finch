69b5dd78c7a1de2b5c9d57d15b8b52ab2265c327
tpetillot <th.petillot@gmail.com>
2022-04-15 18:25:17 +0200

    fix: add defer on readLoop for stack safety

--- core/src/main/scala/io/finch/endpoint/endpoint.scala
+++ core/src/main/scala/io/finch/endpoint/endpoint.scala
@@ -13,12 +13,13 @@ package object endpoint {
 
   private[finch] class FromInputStream[F[_]](stream: Resource[F, InputStream])(implicit F: Async[F]) extends Endpoint[F, Buf] {
 
-    private def readLoop(left: Buf, stream: InputStream): F[Buf] =
+    private def readLoop(left: Buf, stream: InputStream): F[Buf] = F.defer {
       for {
         buffer <- F.delay(new Array[Byte](1024))
         n <- F.blocking(stream.read(buffer))
         buf <- if (n == -1) F.pure(left) else readLoop(left.concat(Buf.ByteArray.Owned(buffer, 0, n)), stream)
       } yield buf
+    }
 
     final def apply(input: Input): Endpoint.Result[F, Buf] =
       EndpointResult.Matched(
